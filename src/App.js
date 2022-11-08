360521af8527bf6b6cd50e3a72bd6ce40b01830b
CODY DE LA TORRE <cdelatorre414@gmail.com>
2022-11-08 11:03:04 -0800

    fixes for full credit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import HornedBeast from './HornedBeast.js';
+import Header from './Header'
+import Main from './Main'
+import Footer from './Footer'
 
 class App extends React.Component {
-  render(){
+  render() {
     return (
       <>
-      <header>
-        <h1>Horned Beasts Here!</h1>
-      </header>
-      <main>
-        <HornedBeast src="https://via.placeholder.com/150" alt="A picture of the flabby beast" name="Scary Horn Flab" description="The scariest flabbiest thing ever known to man. Experts said they don't know why its so flabby or why it has horns. It is just in it's nature to be flabby and have a ton of horns. Pretty wild."/>
-        <HornedBeast src="https://via.placeholder.com/150" alt="A picture of the mobster beast" name="Horned Mobster" description="This mobster has been lurking the wilds of the rainforest for years. Using their horns they hold other animals up for their hard earned food and also threaten them for protection with their wild horns. They are known and feared by other animals for their rackateering and extortion behaviors."/>
-      </main>
-      <footer>Author: Cody De La Torre</footer>
+        <Header></Header>
+        <Main></Main>
+        <Footer></Footer>
       </>
     )
   }
